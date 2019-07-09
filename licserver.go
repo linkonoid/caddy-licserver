93801c9943214519823896ff9bcd209ac550054c
Aaron <44198148+whalehub@users.noreply.github.com>
2019-07-09 17:00:24 +0200

    Update the import paths for Caddy
    Signed-off-by: Aaron <github@jadja.eu>

--- licserver.go
+++ licserver.go
@@ -11,8 +11,8 @@ import (
 	"strconv"
 	"strings"
 	"time"
-	"github.com/mholt/caddy"
-	"github.com/mholt/caddy/caddyhttp/httpserver"
+	"github.com/caddyserver/caddy"
+	"github.com/caddyserver/caddy/caddyhttp/httpserver"
 )
 
 var Configs []Config
